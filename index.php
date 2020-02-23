a43f79ace6cdf752b5d8c308380c1d45c9a56f79
Martinus123S <martinussitinjak225@gmail.com>
2020-02-23 20:06:04 +0700

    Add a jumbotron

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    echo "Hello World";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Biodata Saya</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
 </head>
 <body>
-    <h1>Biodata Saya</h1>
-    <h1>Jangan ada yang ganti ya</h1>
+    <div class="container">
+        <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    </div>
 </body>
 </html>
