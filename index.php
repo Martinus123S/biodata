0dabbc45b4cfd2b4bb971a58ba2f72d2fc875465
Martinus123S <martinussitinjak225@gmail.com>
2020-02-23 20:09:10 +0700

    Create name in jumbotron

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
 </head>
 <body>
-    <div class="container">
-        <div class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <div class="container-fluid">
+        <div class="jumbotron-fluid">
+            <h1 class="display-3" style="text-align: center">Martinus Yudha Chrisanto Sitinjak</h1>
         </div>
+        
     </div>
 </body>
 </html>
